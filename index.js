6aab7e170929f1c90dbf609c016d4ff7a23f6003
Iestyn Williams <iest@me.com>
2015-02-08 18:07:35 +0000

    blacklist ips that already sent a number

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ let bodyParser = require('body-parser');
 let _ = require('underscore');
 let fs = require('fs');
 
+app.enable('trust proxy');
 app.use(bodyParser.json());
 app.set('view engine', 'jade');
+
+let blacklist = [];
 
 let folds = _(fs
   .readFileSync('folds.txt', 'utf8')
@@ -28,10 +31,18 @@ app.get('/', function(req, res) {
 
 app.post('/fold', function(req, res) {
   let fold = req.body.fold;
+  let ip = req.ips[0];
 
-  if (!fold || !Number(fold) || parseInt(fold) > 1e6) {
+  if (blacklist.indexOf(ip)) {
+    return res.sendStatus(401);
+  }
+
+  if (!fold ||
+    !Number(fold) ||
+    parseInt(fold) > 1e6) {
     res.sendStatus(400);
   } else {
+    blacklist.push(ip);
     folds.push(fold);
     fs.appendFile('folds.txt', fold + '\n');
     res.sendStatus(200);
