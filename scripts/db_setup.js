0bd304f284539b59505d7bac75aef7b1c90a576e
Iestyn Williams <iest@me.com>
2020-08-07 14:19:34 +0100

    Change up DB setup script

--- scripts/db_setup.js
+++ scripts/db_setup.js
@@ -1,5 +1,6 @@
 import fs from "fs";
 import faunadb, { query as q } from "faunadb";
+import R from "ramda";
 
 const [, , SECRET] = process.argv;
 
@@ -23,9 +24,11 @@ const allowPreExist = async (cb) => {
   }
 };
 
-const write = async (name, data, client) => {
-  console.log(`> Writing ${data.length} entries to "${name}"`);
-  await allowPreExist(() => client.query(q.CreateCollection({ name })));
+const writeBlacklist = async (data, client) => {
+  console.log(`> Writing ${data.length} entries to blacklist`);
+  await allowPreExist(() =>
+    client.query(q.CreateCollection({ name: "blacklist" }))
+  );
 
   const chunks = chunk(data);
   console.log(`> Got ${chunks.length} chunks`);
@@ -37,22 +40,59 @@ const write = async (name, data, client) => {
         ch,
         q.Lambda(
           "value",
-          q.Create(q.Collection(name), { data: { value: q.Var("value") } })
+          q.Create(q.Collection("blacklist"), {
+            data: { value: q.Var("value") },
+          })
         )
       )
     );
   });
 };
 
+const writeFolds = async (data, client) => {
+  console.log(`> Got ${data.length} folds`);
+  await allowPreExist(() =>
+    client.query(q.CreateCollection({ name: "folds" }))
+  );
+
+  const counted = R.compose(
+    R.map(([fold, count]) => ({ fold, count })),
+    R.toPairs,
+    R.map((arr) => arr.length),
+    R.groupBy(R.identity),
+    R.filter(Boolean)
+  )(data);
+
+  const chunks = chunk(counted);
+  console.log(`> Got ${chunks.length} chunks`);
+
+  chunks.forEach(async (ch, i) => {
+    try {
+      console.log(`> Writing chunk ${i}`);
+      await client.query(
+        q.Map(
+          ch,
+          q.Lambda(
+            "obj",
+            q.Create(q.Collection("folds"), { data: q.Var("obj") })
+          )
+        )
+      );
+    } catch (e) {
+      console.log("> Failed", e);
+    }
+  });
+};
+
 const main = async () => {
   try {
-    const blacklist = read("./blacklist.txt");
+    // const blacklist = read("./blacklist.txt");
     const folds = read("./folds.txt");
 
     const adminClient = new faunadb.Client({ secret: SECRET });
 
-    await write("blacklist", blacklist, adminClient);
-    await write("folds", folds, adminClient);
+    // await writeBlacklist(blacklist, adminClient);
+    await writeFolds(folds, adminClient);
 
     console.log("Done!");
   } catch (e) {
