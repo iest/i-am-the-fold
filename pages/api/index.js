3acbfec4fb2600bdc886156578cfea112ba3b748
Iestyn Williams <iest@me.com>
2020-08-11 12:12:45 +0100

    Blacklisting & fold saving logic

--- pages/api/index.js
+++ pages/api/index.js
@@ -19,14 +19,12 @@ const fetchFolds = async (nextAfter, allData = []) => {
   );
   const newData = allData.concat(data);
   if (after) return await fetchFolds(after, newData);
-  return newData;
+  return newData.map(({ data }) => data);
 };
 
 const fillFold = ({ fold, count }) => Array(count).fill(fold);
 const generateFoldsArr = (foldCounts) =>
-  foldCounts
-    .map(({ data }) => data)
-    .reduce((acc, val) => acc.concat(fillFold(val)), []);
+  foldCounts.reduce((acc, val) => acc.concat(fillFold(val)), []);
 
 const getRandomInt = (min, max) =>
   Math.floor(Math.random() * (max - min) + min);
@@ -48,17 +46,98 @@ const getRandomUniqFolds = (count, arr) => {
 
 const start = async () => {
   if (started) return;
-  const foldCounts = await fetchFolds();
-  folds = generateFoldsArr(foldCounts);
+  folds = await fetchFolds();
   started = true;
+};
+
+const addDBBlacklist = async (value) =>
+  client.query(q.Create(q.Collection("blacklist"), { data: { value } }));
+
+const addDBFold = (data) =>
+  client.query(q.Create(q.Collection("folds"), { data }));
+
+const updateDBFold = async (newfold) => {
+  const { ref, data } = await client.query(
+    q.Get(q.Match(q.Index("folds_by_fold"), newFold))
+  );
+  await client.query(
+    q.Update(q.Ref(ref), {
+      data: {
+        count: data.count + 1,
+      },
+    })
+  );
+};
+
+const addFold = async (newFold, ip) => {
+  try {
+    addDBBlacklist(ip);
+    const existing = folds.find(({ fold }) => fold === newFold);
+    if (existing) {
+      existing.count++;
+      await updateDBFold(addDBFold);
+    } else {
+      const data = { fold: newFold, count: 1 };
+      folds.push(data);
+      await addDBFold(data);
+    }
+  } catch (e) {
+    console.log("Error adding fold", e);
+  }
+};
+
+const queryBlacklist = async (ip) => {
+  try {
+    const blacklisted = await client.query(
+      q.Get(q.Match(q.Index("blacklist_by_ip"), ip))
+    );
+    if (blacklisted) {
+      return true;
+    }
+  } catch (e) {
+    if (e.message === "instance not found") return false;
+    throw e;
+  }
 };
 
 const api = async (req, res) => {
   await start();
-  const subset = getRandomUniqFolds(1000, folds);
+  console.log("> Start");
+
+  if (req.method === "POST") {
+    const {
+      connection: { remoteAddress },
+      body: { fold, challenge, workToken },
+    } = req;
+    console.log("> POST", { fold, challenge, workToken, remoteAddress });
+
+    const blacklisted = await queryBlacklist(remoteAddress);
+
+    if (blacklisted) {
+      res.statusCode = 403;
+      res.end("Fold already saved");
+      return;
+    }
+
+    if (!fold || !Number(fold) || parseInt(fold) > 5120 || parseInt(fold) < 1) {
+      res.statusCode = 400;
+      res.end("Invalid fold");
+      return;
+    }
+    addFold(fold.toString(), remoteAddress);
+    // Check IP against blacklist
+    // Check req has valid token
+    // Create challenge & sign
+
+    // Store used challenges
+    // Store new fold to internal cache & DB
+    // Store new IP to blacklist
+  }
+
+  const subset = getRandomUniqFolds(1000, generateFoldsArr(folds));
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({ folds: subset }));
+  res.end(JSON.stringify({ ip: req.connection.remoteAddress, folds: subset }));
 };
 
 export default api;
--- pages/index.js
+++ pages/index.js
@@ -1,137 +1,163 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Head from "next/head";
-import { Flex, Box } from "rebass";
 
 import Header from "@mdx/header.mdx";
 import Footer from "@mdx/footer.mdx";
 
-const Home = ({ folds, max }) => (
-  <>
-    <Head>
-      <title>I am the fold</title>
-      <meta name="viewport" content="width=device-width initial-scale=1" />
-      <meta
-        name="description"
-        content="An experiment to show how designing for The Fold can be treacherous"
-      />
+const Home = ({ folds, max }) => {
+  const [fold, setFold] = useState();
 
-      <link
-        href="http://fonts.googleapis.com/css?family=Fira+Mono:400,700"
-        rel="stylesheet"
-        type="text/css"
-      />
-    </Head>
+  const saveFold = async (fold) => {
+    try {
+      await fetch("/api", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ fold }),
+      });
+      console.log("Done");
+    } catch (e) {
+      console.log("Error", e);
+    }
+  };
 
-    <style global jsx>{`
-      body,
-      ul {
-        padding: 0;
-        margin: 0;
-      }
-      body {
-        font: 1em/1.4 "Fira Mono", "Menlo", monospace;
-        background: #fff;
-        color: #2a2a2a;
-        text-align: center;
-      }
-      a {
-        color: #333;
-        font-weight: bold;
-      }
-      h1 {
-        margin: 0 0 0.25em;
-      }
-      ul {
-        background: #2a2a2a;
-        color: #fff;
-      }
-      li {
-        position: absolute;
-        list-style: none;
-        border-top: 1px solid #fff;
-        width: 100%;
-        z-index: 1;
-        opacity: 0.1;
-      }
-      li:hover {
-        z-index: 100000;
-        opacity: 1;
-      }
-      li:hover span {
-        background: #fff;
-        color: #2a2a2a;
-      }
-      li span {
-        background: #2a2a2a;
-      }
-      li.current {
-        border-top: 1px solid #ff0000;
-        z-index: 100000;
-        opacity: 1;
-      }
-      li.current:hover span {
-        background: #ff0000;
-        color: #fff;
-      }
-      li.current span {
-        background: #ff0000;
-      }
-      header {
-        padding: 1em;
-        position: relative;
-        z-index: 2;
-        background: #fff;
-      }
-      header p {
-        text-align: left;
-        max-width: 55em;
-        margin: 0 auto 0.5em;
-      }
-      header p span {
-        color: #ff0000;
-      }
-      footer {
-        font-size: 80%;
-        padding: 1em;
-        max-width: 55em;
-        margin: 0 auto;
-      }
-      footer p {
-        text-align: left;
-      }
-      noscript {
-        color: red;
-      }
-    `}</style>
+  useEffect(() => {
+    const fold = window.innerHeight;
+    setFold(fold);
+    saveFold(fold);
+  }, []);
 
-    <header>
-      <Header />
-    </header>
+  return (
+    <>
+      <Head>
+        <title>I am the fold</title>
+        <meta name="viewport" content="width=device-width initial-scale=1" />
+        <meta
+          name="description"
+          content="An experiment to show how designing for The Fold can be treacherous"
+        />
 
-    <ul style={{ height: `${max}px` }}>
-      {folds.map((fold, i) => (
-        <li key={fold + i} style={{ top: `${fold}px` }}>
-          <span>{fold}</span>
-        </li>
-      ))}
-      {typeof window !== "undefined" && window.innerHeight && (
-        <li className="current" style={{ top: `${window.innerHeight}px` }}>
-          <span>{window.innerHeight}</span>
-        </li>
-      )}
-    </ul>
+        <link
+          href="http://fonts.googleapis.com/css?family=Fira+Mono:400,700"
+          rel="stylesheet"
+          type="text/css"
+        />
+      </Head>
 
-    <footer>
-      <Footer />
-    </footer>
-  </>
-);
+      <style global jsx>{`
+        body,
+        ul {
+          padding: 0;
+          margin: 0;
+        }
+        body {
+          font: 1em/1.4 "Fira Mono", "Menlo", monospace;
+          background: #fff;
+          color: #2a2a2a;
+          text-align: center;
+        }
+        a {
+          color: #333;
+          font-weight: bold;
+        }
+        h1 {
+          margin: 0 0 0.25em;
+        }
+        ul {
+          background: #2a2a2a;
+          color: #fff;
+        }
+        li {
+          position: absolute;
+          list-style: none;
+          border-top: 1px solid #fff;
+          width: 100%;
+          z-index: 1;
+          opacity: 0.1;
+        }
+        li:hover {
+          z-index: 100000;
+          opacity: 1;
+        }
+        li:hover span {
+          background: #fff;
+          color: #2a2a2a;
+        }
+        li span {
+          background: #2a2a2a;
+        }
+        li.current {
+          border-top: 1px solid #ff0000;
+          z-index: 100000;
+          opacity: 1;
+        }
+        li.current:hover span {
+          background: #ff0000;
+          color: #fff;
+        }
+        li.current span {
+          background: #ff0000;
+        }
+        header {
+          padding: 1em;
+          position: relative;
+          z-index: 2;
+          background: #fff;
+        }
+        header p {
+          text-align: left;
+          max-width: 55em;
+          margin: 0 auto 0.5em;
+        }
+        header p span {
+          color: #ff0000;
+        }
+        footer {
+          font-size: 80%;
+          padding: 1em;
+          max-width: 55em;
+          margin: 0 auto;
+        }
+        footer p {
+          text-align: left;
+        }
+        noscript {
+          color: red;
+        }
+      `}</style>
+
+      <header>
+        <Header />
+      </header>
+
+      <ul style={{ height: `${max}px` }}>
+        {folds.map((fold, i) => (
+          <li key={fold + i} style={{ top: `${fold}px` }}>
+            <span>{fold}</span>
+          </li>
+        ))}
+        {fold && (
+          <li className="current" style={{ top: `${fold}px` }}>
+            <span>{fold}</span>
+          </li>
+        )}
+      </ul>
+
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  );
+};
 
 export async function getServerSideProps({ req }) {
   const protocol = req.connection.encrypted ? "https" : "http";
   const baseUrl = req ? `${protocol}://${req.headers.host}` : "";
+
   const { folds } = await fetch(baseUrl + "/api").then((res) => res.json());
   const max = folds.reduce((a, b) => Math.max(a, b));
+
   return { props: { folds, max } };
 }
 
